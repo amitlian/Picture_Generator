abfb25b4ad2fedb8a9ca418ed1d5b14acf8fabf2
Amit <lianbadhon@gmail.com>
2020-09-10 23:25:24 +0600

    init

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flexId');
+    image.src = "https://thumbs.gfycat.com/PlaintiveBlackAtlanticspadefish-size_restricted.gif";
+    div.appendChild(image);
+}
